de8988458f2bdf5799a6afedcc81f5650ae9d1b3
Sachin Cherian <sachinctl@protonmail.com>
2023-05-18 15:07:14 +0530

    try hand vectorizing sliding move generation
    with avx2 intrinsics

--- crates/maven/benches/bishop.rs
+++ crates/maven/benches/bishop.rs
@@ -1,11 +1,7 @@
-#![feature(portable_simd)]
-
-use std::simd::u64x4;
-
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use sealion_board::{Position, Square};
-use sealion_maven::{partial_diagonal_shadow_mask, Maven, chunk_diagonal_shadow_mask,};
+use sealion_maven::Maven;
 
 pub fn benchmark_bishop(c: &mut Criterion) {
     c.bench_function("Bench Generate Bishop Moves", |b| {
@@ -22,32 +18,5 @@ pub fn benchmark_bishop(c: &mut Criterion) {
     });
 }
 
-pub fn benchmark_shadow(c: &mut Criterion) {
-    c.bench_function("Shadow Mask", |b| {
-        b.iter(|| {
-            black_box(Maven::diagonal_shadow_or(black_box(u64x4::splat(5))));
-        })
-    });
-
-    c.bench_function("Partial Single Shadow Mask", |b| {
-        b.iter(|| {
-            black_box(partial_diagonal_shadow_mask(
-                black_box(2),
-                black_box(83727832),
-                black_box(33),
-            ));
-        })
-    });
-
-    c.bench_function("Single Shadow Mask 8", |b| {
-        b.iter(|| {
-            black_box(chunk_diagonal_shadow_mask(
-                black_box(u64x4::splat(32)),
-                black_box(32),
-            ));
-        })
-    });
-}
-
-criterion_group!(benches, benchmark_bishop, benchmark_shadow);
+criterion_group!(benches, benchmark_bishop);
 criterion_main!(benches);
--- crates/maven/src/lib.rs
+++ crates/maven/src/lib.rs
@@ -1,72 +1,152 @@
 //! Move generator implementation.
 
-#![feature(portable_simd)]
-#![feature(const_trait_impl)]
-
-use std::simd::{u64x4, u64x8, SimdUint};
+use std::cmp::min;
 
 use sealion_board::{BitBoard, Square};
-
-pub mod tables;
 
 /// The primary structure which contains relevant piece state information, such as attacks and checks.
 pub struct Maven {}
 
 impl Maven {
     pub fn bishop_moves(square: Square, friendly: BitBoard, unfriendly: BitBoard) -> BitBoard {
-        let mut quasi_move_table = u64x4::splat(0);
+        let max_moves = [
+            min(7 - square.rank(), 7 - square.file()) as i8,
+            min(7 - square.rank(), square.file()) as i8,
+            min(square.rank(), 7 - square.file()) as i8,
+            min(square.rank(), square.file()) as i8,
+        ];
 
-        for i in 0..4 {
-            quasi_move_table[i] = tables::DIAGONAL_SHADOWS[i][square.raw_index() as usize].0;
+        if is_x86_feature_detected!("avx2") {
+            unsafe {
+                Self::sliding_moves_avx2(
+                    square,
+                    friendly,
+                    unfriendly,
+                    [9, 7, 0, 0],
+                    [0, 0, 7, 9],
+                    max_moves
+                )
+            }
+        } else {
+            Self::sliding_moves(square, friendly, unfriendly, [9, 7, -7, -9], max_moves)
+        }
+    }
+
+    fn sliding_moves(
+        square: Square,
+        friendly: BitBoard,
+        unfriendly: BitBoard,
+        shifts: [i8; 4],
+        max_moves: [i8; 4]
+    ) -> BitBoard {
+        let mut moves = [0; 4];
+
+        for direction in 0..4 {
+            for index in 1..(max_moves[direction] + 1) {
+                let square = BitBoard(1 << (square.raw_index() as i8 + (index * shifts[direction])));
+
+                if square & friendly != 0 {
+                    break;
+                }
+
+                moves[direction] |= square.0;
+
+                if square & unfriendly != 0 {
+                    break;
+                }
+            }
         }
 
-        let quasi_moves = BitBoard(quasi_move_table.reduce_or());
-
-        // friendly blockers
-        let all_blockers = quasi_move_table & u64x4::splat((friendly | unfriendly).0);
-        let our_blockers = quasi_moves & friendly;
-
-        let shadow = Self::diagonal_shadow_or(all_blockers) | our_blockers;
-
-        quasi_moves ^ shadow
+        BitBoard(moves[0] | moves[1] | moves[2] | moves[3])
     }
 
-    /// Generate diagonal shadow across only one direction.
-    pub fn diagonal_shadow_or(blockers: u64x4) -> BitBoard {
-        let mut shadow = u64x8::splat(0);
+    #[target_feature(enable = "avx2")]
+    unsafe fn sliding_moves_avx2(
+        square: Square,
+        friendly: BitBoard,
+        unfriendly: BitBoard,
+        lshifts: [i8; 4],
+        rshifts: [i8; 4],
+        max_moves: [i8; 4]
+    ) -> BitBoard {
+        use core::arch::x86_64::*;
+        use std::mem::transmute;
 
-        for index in 0..8 {
-            shadow[index] = chunk_diagonal_shadow_mask(blockers, index * 8);
+        // Does:
+        // a &= !(mask)((b & c) == c)
+        let is_set_mask = |a: __m256i, b: __m256i, c: __m256i| -> __m256i {
+            _mm256_andnot_si256(_mm256_cmpeq_epi64(_mm256_and_si256(b, c), c), a)
+        };
+
+        // Does
+        // a &= (mask)(b < c)
+        let and_lte_mask = |a: __m256i, b: __m256i, c: __m256i| -> __m256i {
+            _mm256_andnot_si256(_mm256_cmpgt_epi64(b, c), a)
+        };
+
+        // Does
+        // a |= b & c
+        let or_and_mask = |a: __m256i, b: __m256i, c: __m256i| -> __m256i {
+            _mm256_or_si256(a, _mm256_and_si256(b, c))
+        };
+
+        // Horizontal OR reduction to 64 bits
+        let reduce_or = |a: __m256i| -> u64 {
+            // OR across 128 bits
+            let low_128 = _mm256_extracti128_si256::<0>(a);
+            let high_128 = _mm256_extracti128_si256::<1>(a);
+            let or_128 = _mm_or_si128(low_128, high_128);
+            // OR across 64 bits
+            let low_64 = _mm_extract_epi64::<0>(or_128);
+            let high_64 = _mm_extract_epi64::<1>(or_128);
+
+            transmute(low_64 | high_64)
+        };
+
+        // Compute max iterations
+        let max_moves = max_moves.map(i64::from);
+        let max_moves = _mm256_loadu_si256(max_moves.as_ptr() as *const _);
+
+        // Direction iterator progress
+        let lshifts = lshifts.map(i64::from);
+        let rshifts = rshifts.map(i64::from);
+
+        let lshifts = _mm256_loadu_si256(lshifts.as_ptr() as *const _);
+        let rshifts = _mm256_loadu_si256(rshifts.as_ptr() as *const _);
+
+        // Full move set
+        let mut moves = _mm256_set1_epi64x(0);
+
+        // Player masks
+        let friendly = _mm256_set1_epi64x(transmute(friendly.0));
+        let unfriendly = _mm256_set1_epi64x(transmute(unfriendly.0));
+
+        // Direction enabled mask
+        let mut enabled = _mm256_set1_epi64x(!0);
+
+        // Isolated square iterator
+        let mut square = _mm256_set1_epi64x(1 << square.raw_index());
+
+        for index in 0..7 {
+            // Get the next set of squares in the loop
+            square = _mm256_sllv_epi64(square, lshifts);
+            square = _mm256_srlv_epi64(square, rshifts);
+
+            // Check if max moves exceeded
+            enabled = and_lte_mask(enabled, _mm256_set1_epi64x(index + 1), max_moves);
+
+            // Check for friendly blocker
+            enabled = is_set_mask(enabled, friendly, square);
+
+            // Register move
+            moves = or_and_mask(moves, square, enabled);
+
+            // Check for unfriendly blocker
+            enabled = is_set_mask(enabled, unfriendly, square);
         }
 
-        BitBoard(shadow.reduce_or())
+        BitBoard(reduce_or(moves))
     }
-}
-
-pub fn partial_diagonal_shadow_mask(direction: usize, blockers: u64, index: usize) -> u64 {
-    // check if the current square
-    // 1) blocks movement (contains a piece)
-    // 2) is only in the direction of movement
-    let valid_shadow = (blockers >> index) & 1;
-
-    tables::DIAGONAL_SHADOWS[direction][index].0 & !(valid_shadow.wrapping_sub(1))
-}
-
-pub fn chunk_partial_diagonal_shadow_mask(direction: usize, blockers: u64, index: usize) -> u64 {
-    let mut mask = u64x8::splat(0);
-
-    for shift in 0..8 {
-        mask[shift] = partial_diagonal_shadow_mask(direction, blockers, index + shift);
-    }
-
-    mask.reduce_or()
-}
-
-pub fn chunk_diagonal_shadow_mask(blockers: u64x4, index: usize) -> u64 {
-    chunk_partial_diagonal_shadow_mask(0, blockers[0], index) |
-    chunk_partial_diagonal_shadow_mask(1, blockers[1], index) |
-    chunk_partial_diagonal_shadow_mask(2, blockers[2], index) |
-    chunk_partial_diagonal_shadow_mask(3, blockers[3], index)
 }
 
 #[cfg(test)]
